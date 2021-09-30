aceca8be16b869bd44c74ea1ee0ea1432c053c46
Vladimir Bumbic <vladimirbumbic@gmail.com>
2021-09-30 18:25:51 +0200

    .

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let inputMail = document.getElementById("mail");
-let submitBtn = document.getElementById("submitEmail");
-let divError = document.getElementById("error");
+let inputMail = document.getElementById('mail');
+let submitBtn = document.getElementById('submitEmail');
+let divError = document.getElementById('error');
 let regex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-submitBtn.addEventListener("click", (e) => {
+submitBtn.addEventListener('click', e => {
   e.preventDefault();
 
-  if (inputMail.value === "") {
-    divError.innerHTML = "Oops! Please add your email";
+  if (inputMail.value === '') {
+    divError.innerHTML = 'Oops! Please add your email';
   } else if (!inputMail.value.match(regex)) {
-    divError.innerHTML = "Oops! Please check your email";
+    divError.innerHTML = 'Oops! Please check your email';
   }
 });
