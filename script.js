90420987dbcae7125006c27ab02fb5c5dc7d5a98
Vladimir Bumbic <vladimirbumbic@gmail.com>
2021-10-01 13:31:26 +0200

    .

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let inputMail = document.getElementById('mail');
 let submitBtn = document.getElementById('submitEmail');
 let divError = document.getElementById('error');
 let regex =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  if (inputMail.value === '') {
-    divError.innerHTML = 'Oops! Please add your email';
-  } else if (!inputMail.value.match(regex)) {
-    divError.innerHTML = 'Oops! Please check your email';
-  }
+    if (inputMail.value === '') {
+        divError.innerHTML = 'Oops! Please add your email';
+    } else if (!inputMail.value.match(regex)) {
+        divError.innerHTML = 'Oops! Please check your email';
+    }
 });
